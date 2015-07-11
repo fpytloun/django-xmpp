85d40e308643347041810ce9e8d20db997163ee7
Filip Pytloun <filip@pytloun.cz>
2015-07-11 13:54:19 +0200

    Use latest conversejs build

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ f.close()
 
 setup(
     name='django-xmpp',
-    version='0.3',
+    version='0.3.1',
     description='XMPP integration for Django app made simple',
     long_description=readme,
     author="Filip Pytloun",
